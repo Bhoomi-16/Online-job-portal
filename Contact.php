3c08918c84537143a1fda18e1471933bd02b352f
Bhoomika T N <bhoomikatn1609@gmail.com>
2024-11-29 19:46:47 +0530

    first commit

---
+++ Contact.php
@@ -0,0 +1,55 @@
+ 
+	<section id="content">
+	
+	<div class="container">
+		<div class="row"> 
+							<div class="col-md-12">
+								<div class="about-logo">
+									<h3>E Career Gateway</h3>
+									<p>E Career Gateway is the #1 job site in the world1 with over 25 crore unique visitors2 every month. E Career strives to put job seekers first, giving them free access to search for jobs, post resumes, and research companies. Every day, we connect millions of people to new opportunities</p>
+                                    	<p>At E Career, our mission is to help people get jobs. We have more than 10,000 global employees passionately pursuing this purpose and improving the recruitment journey through real stories and data. We foster a collaborative workplace that strives to create the best experience for job seekers.</p>
+								</div>  
+							</div>
+						</div>
+	<div class="row">
+								<div class="col-md-6">
+									<p></p>
+								  	
+		   <!-- Form itself -->
+          <form name="sentMessage" id="contactForm"  novalidate>
+	       <h3>Contact me</h3>
+		 <div class="control-group">
+                    <div class="controls">
+			<input type="text" class="form-control" 
+			   	   placeholder="Full Name" id="name" required
+			           data-validation-required-message="Please enter your name" />
+			  <p class="help-block"></p>
+		   </div>
+	         </div> 	
+                <div class="control-group">
+                  <div class="controls">
+			<input type="email" class="form-control" placeholder="Email" 
+			   	            id="email" required
+			   		   data-validation-required-message="Please enter your email" />
+		</div>
+	    </div> 	
+			  
+               <div class="control-group">
+                 <div class="controls">
+				 <textarea rows="10" cols="100" class="form-control" 
+                       placeholder="Message" id="message" required
+		       data-validation-required-message="Please enter your message" minlength="5" 
+                       data-validation-minlength-message="Min 5 characters" 
+                        maxlength="999" style="resize:none"></textarea>
+		</div>
+               </div> 		 
+	     <div id="success"> </div> <!-- For success/fail messages -->
+	    <button type="submit" class="btn btn-primary pull-right">Send</button><br />
+          </form>
+								</div>
+								
+							</div>
+	</div>
+ 
+	</section>
+ 
